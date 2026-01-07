f3ff360926987a6162686a85099d31a0f0625c56
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-01-06 22:27:20 -0300

    feat: finaliza sistema de propostas em memória

--- src/br/com/banco/app/Main.java
+++ src/br/com/banco/app/Main.java
@@ -1,7 +1,11 @@
 package br.com.banco.app;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import br.com.banco.exception.NegocioException;
 import br.com.banco.model.PropostaEmprestimo;
 import br.com.banco.service.ValidadorEmprestimo;
 
@@ -10,26 +14,34 @@ public class Main {
 
     public static void main(String[] args) {
         String continuar;
-        ValidadorEmprestimo validarEmprestimo = new ValidadorEmprestimo();
+        List<PropostaEmprestimo> historico = new ArrayList<>();
+        ValidadorEmprestimo validador = new ValidadorEmprestimo();
 
         do {
-            System.out.println("Insira seu nome: ");
-            String nomeCliente = scan.nextLine().trim();
+            boolean dadosValidos = false;
+            while(!dadosValidos) {
+                try {
+                    System.out.println("Insira seu nome: ");
+                    String nomeCliente = scan.nextLine().trim();
+                    BigDecimal salarioBruto = lerBigDecimalSeguro("Insira o seu salário bruto: ");
+                    BigDecimal valorPrestacao = lerBigDecimalSeguro("Insira o valor da prestação: ");
 
-            BigDecimal salarioBruto = lerBigDecimalSeguro("Insira o seu salário bruto: ");
-            BigDecimal valorPrestacao = lerBigDecimalSeguro("Insira o valor da prestação: ");
+                    PropostaEmprestimo proposta = new PropostaEmprestimo(nomeCliente, salarioBruto, valorPrestacao);
+                    dadosValidos = true;
+                    historico.add(proposta);
+                    exibirAprovacao(proposta, validador);
 
-            PropostaEmprestimo minhaProposta = new PropostaEmprestimo(nomeCliente, salarioBruto, valorPrestacao);
-
-            if(validarEmprestimo.verificarAprovacao(minhaProposta)){
-                System.out.println("O empréstimo foi Aprovado! ");
-            } else {
-                System.out.println("STATUS: Empréstimo Negado! Valor máximo permitido: " + validarEmprestimo.calcularLimite(minhaProposta));
+                } catch (NegocioException e) {
+                    System.out.println("ATENÇÃO: " + e.getMessage());
+                }
             }
-
             System.out.println("Deseja continuar? [S/N]: ");
             continuar = scan.nextLine();
         } while (continuar.equalsIgnoreCase("S"));
+        for(PropostaEmprestimo emprestimos: historico) {
+            String status = validador.verificarAprovacao(emprestimos) ? "Aprovado" : "Reprovado";
+            System.out.println("Cliente: " + emprestimos.getNomeCliente() + " | Salário: " + emprestimos.getSalarioBruto() + " | Status: " + status);
+        }
     }
 
     /*
@@ -51,8 +63,16 @@ public class Main {
 
             } catch (NumberFormatException e) {
                 System.out.println("ERRO: Digite um valor numérico válido (ex: 1500.50)!");
-                scan.nextLine();
             }
         } while(true);
+    }
+
+    private static void exibirAprovacao(PropostaEmprestimo proposta, ValidadorEmprestimo validador) {
+        if (validador.verificarAprovacao(proposta)) {
+            System.out.println("O empréstimo foi Aprovado! ");
+        } else {
+            System.out.println("STATUS: Empréstimo Negado! \nValor máximo permitido: " + validador.calcularLimite(proposta));
+        }
+
     }
 }
--- src/br/com/banco/model/PropostaEmprestimo.java
+++ src/br/com/banco/model/PropostaEmprestimo.java
@@ -1,5 +1,7 @@
 package br.com.banco.model;
 
+import br.com.banco.exception.NegocioException;
+
 import java.math.BigDecimal;
 
 public class PropostaEmprestimo {
@@ -8,20 +10,19 @@ public class PropostaEmprestimo {
     private final BigDecimal valorPrestacao;
 
     public PropostaEmprestimo(String nomeCliente, BigDecimal salarioBruto, BigDecimal valorPrestacao){
+        if (nomeCliente == null || nomeCliente.isBlank()) throw new NegocioException("Insira um nome Válido!!!");
         this.nomeCliente = nomeCliente;
+        if (salarioBruto == null || salarioBruto.compareTo(BigDecimal.ZERO) <= 0)
+            throw new NegocioException("O salário bruto deve ser maior que zero!");
         this.salarioBruto = salarioBruto;
+        if (valorPrestacao == null || valorPrestacao.compareTo(BigDecimal.ZERO) <= 0)
+            throw new NegocioException("A prestação deve ser maior que zero!");
         this.valorPrestacao = valorPrestacao;
     }
 
-    public String getNomeCliente(){
-        return nomeCliente;
-    }
-    public BigDecimal getSalarioBruto(){
-        return salarioBruto;
-    }
-    public BigDecimal getValorPrestacao(){
-        return valorPrestacao;
-    }
+    public String getNomeCliente() { return nomeCliente; }
+    public BigDecimal getSalarioBruto(){ return salarioBruto; }
+    public BigDecimal getValorPrestacao(){ return valorPrestacao; }
 
 }
 
