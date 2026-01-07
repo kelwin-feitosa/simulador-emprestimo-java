3977524da388bf3f1d48b193b3d344dae8df1889
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-01-07 16:34:28 -0300

    style: melhora a legibilidade e o espaçamento da classe PropostaEmprestimo

--- src/br/com/banco/model/PropostaEmprestimo.java
+++ src/br/com/banco/model/PropostaEmprestimo.java
@@ -10,11 +10,15 @@ public class PropostaEmprestimo {
     private final BigDecimal valorPrestacao;
 
     public PropostaEmprestimo(String nomeCliente, BigDecimal salarioBruto, BigDecimal valorPrestacao){
-        if (nomeCliente == null || nomeCliente.isBlank()) throw new NegocioException("Insira um nome Válido!!!");
+
+        if (nomeCliente == null || nomeCliente.isBlank())
+            throw new NegocioException("Insira um nome Válido!!!");
         this.nomeCliente = nomeCliente;
+        
         if (salarioBruto == null || salarioBruto.compareTo(BigDecimal.ZERO) <= 0)
             throw new NegocioException("O salário bruto deve ser maior que zero!");
         this.salarioBruto = salarioBruto;
+
         if (valorPrestacao == null || valorPrestacao.compareTo(BigDecimal.ZERO) <= 0)
             throw new NegocioException("A prestação deve ser maior que zero!");
         this.valorPrestacao = valorPrestacao;
